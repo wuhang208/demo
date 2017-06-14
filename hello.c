e7318296d75abf04396df95819c6ea5d62dbbba1
wuhang <15733173176@163.com>
2017-06-14 15:26:58 +0800

    Forty hello.c
    forty use hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 
 int main(void){
-    printf("welcome sjzue\n");
+    int i;
+    int a=2;
+    for (i=0;i<4;i++){
+        a=a*a;
+    }
+    printf("a=%d\n",a);
     return 0;
 }
